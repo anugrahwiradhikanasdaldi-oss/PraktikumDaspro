ce70fc887b170de680d6af6b71d5ebab6057a98a
Nasdaldi <anugrahwiradhikanasdaldi@gmail.com>
2025-10-01 10:17:43 +0700

    tugas 2

---
+++ jobsheet5/FlowCardno18.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class FlowCardno18 {
+  public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int angka;
+        System.out.print("Masukkan sebuah angka: ");
+        angka = input.nextInt();
+        if (angka % 2 == 0) {
+            System.out.println("Genap");
+        } else {
+            System.out.println("Ganjil");
+        }
+        input.close();
+    }
+}  

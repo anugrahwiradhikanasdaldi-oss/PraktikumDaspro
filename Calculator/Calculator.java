decccdbdd69e3eb30080494ca7ff967f1dcee627
Nasdaldi <anugrahwiradhikanasdaldi@gmail.com>
2025-10-17 12:50:52 +0700

    Add files via upload

---
+++ Calculator/Calculator.java
@@ -0,0 +1,14 @@
+package Calculator; // Mendeklarasikan bahwa kelas ini berada di dalam package bernama "Calculator"
+
+public class Calculator {
+
+    // Method untuk menjumlahkan dua angka
+    // Parameter:
+    //  - a: angka pertama
+    //  - b: angka kedua
+    // Return:
+    //  - hasil penjumlahan dari a + b
+    public int add(int a, int b) {
+        return a + b; // Mengembalikan hasil penjumlahan
+    }
+}
